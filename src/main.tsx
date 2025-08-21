3d4facb7b13848f5e8fabd47064bd686c3e268c9
Arturo Verbel <arturo-verbel@hotmail.com>
2025-08-20 22:53:45 -0500

    More redirects

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+import Accounts from './pages/Accounts.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/accounts',
+    element: <Accounts />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
